91fefa0fccde582f5ad3d223229df78b15d291a0
Kim Yewon <kyw017763@gmail.com>
2021-05-16 13:42:58 +0900

    Use data mapper pattern

--- index.ts
+++ index.ts
@@ -1,24 +1,12 @@
-import * as bcryptjs from 'bcryptjs';
 import { createConnection } from 'typeorm';
-import Account from './entity/Account';
 import ormConfig from './ormConfig'
+import AccountRepository from './repository/AccountRepository';
 
 createConnection(ormConfig)
     .then(async connection => {
-        const account = new Account();
-        
-        account.email = 'kyw017763@naver.com';
-        account.password = bcryptjs.hashSync('123123');
-        account.username = '김예원';
-        account.type = 'local';
-        account.notificationOpen = true;
-        account.notificationOpenDate = new Date();
-        account.emailOpen = true;
-        account.emailOpenDate = new Date();
-        account.lastUpdateIp = '';
-        account.createIp = '';
-        account.active = true;
-
-        await connection.manager.save(account);
+        const accountRepository = connection.getCustomRepository(AccountRepository);
+        await accountRepository.Create();
+        const accounts = await accountRepository.GetList();
+        console.log(accounts);
     })
     .catch(err => console.log(err));
---
+++ repository/AccountRepository.ts
@@ -0,0 +1,33 @@
+import * as bcryptjs from 'bcryptjs';
+import { EntityRepository, Repository } from 'typeorm';
+import Account from '../entity/Account';
+
+@EntityRepository(Account)
+export default class AccountRepository extends Repository<Account> {
+    Create() {
+        const account = new Account();
+        
+        account.email = 'kyw017763@naver.com';
+        account.password = bcryptjs.hashSync('123123');
+        account.username = '김예원';
+        account.type = 'local';
+        account.notificationOpen = true;
+        account.notificationOpenDate = new Date();
+        account.emailOpen = true;
+        account.emailOpenDate = new Date();
+        account.lastUpdateIp = '';
+        account.createIp = '';
+        account.active = true;
+
+        return this.createQueryBuilder('account')
+            .insert()
+            .into(Account)
+            .values(account)
+            .execute();
+    }
+
+    GetList() {
+        return this.createQueryBuilder('account')
+            .getMany();
+    }
+}
